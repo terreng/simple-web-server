991111472f66f5e073858f464905104edc237618
Terren <11605395+terreng@users.noreply.github.com>
2021-07-13 09:22:09 -0700

    Add open source licenses

--- index.js
+++ index.js
@@ -122,7 +122,23 @@ function createServer(serverconfig) {
 		console.error(err);
 	});
 	server.listen(serverconfig.port, 'localhost');
-	enableDestroy(server);
+
+	var connections = {}
+
+	server.on('connection', function(conn) {
+	  var key = conn.remoteAddress + ':' + conn.remotePort;
+	  connections[key] = conn;
+	  conn.on('close', function() {
+		delete connections[key];
+	  });
+	});
+  
+	server.destroy = function(cb) {
+	  server.close(cb);
+	  for (var key in connections)
+		connections[key].destroy();
+	};
+
 	console.log("started")
 
 	servers.push(server);
@@ -131,22 +147,4 @@ function createServer(serverconfig) {
 
 }
 
-}
-
-function enableDestroy(server) {
-  var connections = {}
-
-  server.on('connection', function(conn) {
-    var key = conn.remoteAddress + ':' + conn.remotePort;
-    connections[key] = conn;
-    conn.on('close', function() {
-      delete connections[key];
-    });
-  });
-
-  server.destroy = function(cb) {
-    server.close(cb);
-    for (var key in connections)
-      connections[key].destroy();
-  };
 }
