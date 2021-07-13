3c217a11997044d2f05f9b3969cfa9002bc63499
Terren <11605395+terreng@users.noreply.github.com>
2021-07-12 17:52:26 -0700

    Start working on actual http server

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const {app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const fs = require('fs');
+const http = require('http');
 
 let mainWindow;
 var config = {};
 
 app.on('ready', function() {
+	try {
+		config = JSON.parse(fs.readFileSync(path.join(app.getPath('userData'), "config.json")));
+	} catch(error) {
+		config = {};
+	}
 	createWindow();
+	startServers();
 })
 
 app.on('window-all-closed', function () {
@@ -27,6 +34,7 @@ ipcMain.on('quit', function(event) {
 ipcMain.on('saveconfig', function(event, arg1) {
 	fs.writeFileSync(path.join(app.getPath('userData'), "config.json"), JSON.stringify(arg1), "utf8");
 	config = arg1;
+	startServers();
 })
 
 app.on('activate', function () {
@@ -62,12 +70,6 @@ function createWindow() {
 	//mainWindow.webContents.openDevTools();
 	
 	mainWindow.webContents.on('did-finish-load', function() {
-		try {
-			config = JSON.parse(fs.readFileSync(path.join(app.getPath('userData'), "config.json")));
-		} catch(error) {
-			config = {};
-		}
-
 		mainWindow.webContents.send('message', config);
 	});
 
@@ -75,4 +77,76 @@ function createWindow() {
 		mainWindow = null
 	});
 
+}
+
+var servers = [];
+
+function startServers() {
+
+if (servers.length > 0) {
+var closed_servers = 0;
+for (var i = 0; i < servers.length; i++) {
+	servers[i].close(function(err) {
+		checkServersClosed()
+	});
+	servers[i].destroy();
+}
+function checkServersClosed() {
+	closed_servers++;
+	if (closed_servers == servers.length) {
+		servers = [];
+		createServers()
+	}
+}
+} else {
+	createServers()
+}
+
+function createServers() {
+
+for (var i = 0; i < (config.servers || []).length; i++) {
+
+createServer(config.servers[i]);
+
+}
+
+function createServer(serverconfig) {
+
+	const server = http.createServer(function (req, res) {
+		//console.log(req);
+		res.writeHead(200, { 'Content-Type': "text/plain; charset=utf-8", 'Content-Length': Buffer.byteLength("hello", "utf-8") });
+		res.write("hello", "utf-8");
+		res.end();
+	});
+	server.on('error', function(err) {
+		console.error(err);
+	});
+	server.listen(serverconfig.port, 'localhost');
+	enableDestroy(server);
+	console.log("started")
+
+	servers.push(server);
+
+}
+
+}
+
+}
+
+function enableDestroy(server) {
+  var connections = {}
+
+  server.on('connection', function(conn) {
+    var key = conn.remoteAddress + ':' + conn.remotePort;
+    connections[key] = conn;
+    conn.on('close', function() {
+      delete connections[key];
+    });
+  });
+
+  server.destroy = function(cb) {
+    server.close(cb);
+    for (var key in connections)
+      connections[key].destroy();
+  };
 }
