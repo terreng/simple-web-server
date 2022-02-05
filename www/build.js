ec9146261d1c215b097cf7f12cc7cad490692de2
Terren <11605395+terreng@users.noreply.github.com>
2022-02-05 13:28:07 -0500

    Versions URL

--- www/build.js
+++ www/build.js
@@ -10,3 +10,14 @@ fs.copyFileSync("www/index.html", "www/out/index.html");
 fs.copyFileSync("www/style.css", "www/out/style.css");
 
 //convert to markdown and fix links
+
+
+
+//Generate versions json
+if (!fs.existsSync("www/out/versions")){
+    fs.mkdirSync("www/out/versions");
+}
+var versions = JSON.parse(fs.readFileSync("www/versions.json", "utf8"));
+for (var i = 0; i < Object.keys(versions.versions).length; i++) {
+    fs.writeFileSync("www/out/versions/"+Object.keys(versions.versions)[i]+".json", JSON.stringify((Object.keys(versions.versions)[i] !== versions.latest) ? {"update": true, "version": versions.latest, "name": versions.versions[versions.latest].name, "download": versions.versions[versions.latest].download} : {"update": false}), "utf8");
+}
