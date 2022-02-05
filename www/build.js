f68a6c7f5f355ff6697d060e916f0660540d064a
Terren <11605395+terreng@users.noreply.github.com>
2022-02-05 11:46:08 -0500

    Add build script

---
+++ www/build.js
@@ -0,0 +1,10 @@
+var markdown = require("markdown").markdown;
+var fs = require("fs");
+var path = require("path");
+
+//Copy static files
+if (!fs.existsSync("www/out")){
+    fs.mkdirSync("www/out");
+}
+fs.copyFileSync("www/index.html", "www/out/index.html");
+fs.copyFileSync("www/style.css", "www/out/style.css");
