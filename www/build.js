96b38f4395d772c6b34ea27e707dfcfea080f0a1
Terren <11605395+terreng@users.noreply.github.com>
2022-02-05 12:03:41 -0500

    Update build.js

--- www/build.js
+++ www/build.js
@@ -8,3 +8,5 @@ if (!fs.existsSync("www/out")){
 }
 fs.copyFileSync("www/index.html", "www/out/index.html");
 fs.copyFileSync("www/style.css", "www/out/style.css");
+
+//convert to markdown and fix links
