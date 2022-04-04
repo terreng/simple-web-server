9023d481b1d936174d40461cf4e0f44126afb51d
Terren <11605395+terreng@users.noreply.github.com>
2022-04-03 23:00:04 -0400

    Add favicon

--- www/src/.vuepress/config.js
+++ www/src/.vuepress/config.js
@@ -4,7 +4,12 @@ module.exports = {
   title: 'Simple Web Server',
   description: description,
   head: [
-    ['meta', { name: 'theme-color', content: '#d09608' }]
+    ['meta', { name: 'theme-color', content: '#d09608' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#d09608' }],
+    ['link', { rel: 'manifest', href: '/site.webmanifest' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
   ],
   themeConfig: {
     sidebarDepth: 3,
