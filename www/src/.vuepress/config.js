8797c2848ff0237c1360a8d8f81612a7b412e23b
Terren <11605395+terreng@users.noreply.github.com>
2022-04-04 11:38:14 -0400

    add keywords meta tag

--- www/src/.vuepress/config.js
+++ www/src/.vuepress/config.js
@@ -10,7 +10,8 @@ module.exports = {
     ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }],
     ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
     ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
-    ['link', { rel: 'shortcut icon', href: '/favicon.ico' }]
+    ['link', { rel: 'shortcut icon', href: '/favicon.ico' }],
+    ['meta', { name: 'keywords', content: 'Simple Web Server,web server,Web Server for Chrome,local web server' }]
   ],
   themeConfig: {
     sidebarDepth: 3,
