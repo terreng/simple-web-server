aa48b84498092c6081c29ef328ddeab0bb68d8e4
Terren <11605395+terreng@users.noreply.github.com>
2022-04-04 11:31:41 -0400

    Favicon

--- www/build.js
+++ www/build.js
@@ -8,7 +8,7 @@ var versions = JSON.parse(fs.readFileSync("versions.json", "utf8"));
 for (var i = 0; i < Object.keys(versions.versions).length; i++) {
     fs.writeFileSync("src/.vuepress/dist/versions/"+Object.keys(versions.versions)[i]+".json", JSON.stringify((Object.keys(versions.versions)[i] !== versions.latest) ? {"update": true, "version": versions.latest, "name": versions.versions[versions.latest].name, "download": versions.versions[versions.latest].download} : {"update": false}), "utf8");
 }
-
+/*
 var static_files = [
     "android-chrome-192x192.png",
     "android-chrome-512x512.png",
@@ -22,4 +22,4 @@ var static_files = [
 ]
 for (var i = 0; i < static_files.length; i++) {
     fs.copyFileSync("src/"+static_files[i], "src/.vuepress/dist/"+static_files[i]);
-}
+}*/
--- www/src/.vuepress/config.js
+++ www/src/.vuepress/config.js
@@ -10,6 +10,7 @@ module.exports = {
     ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }],
     ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
     ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
+    ['link', { rel: 'shortcut icon', href: '/favicon.ico' }]
   ],
   themeConfig: {
     sidebarDepth: 3,
