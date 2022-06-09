0500c4820ef6e428e0b6bfe6ee89dacb9b9df2df
Terren <11605395+terreng@users.noreply.github.com>
2022-06-08 19:55:29 -0700

    macos app store build

---
+++ build/notarize.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const { notarize } = require('electron-notarize');
+
+exports.default = async function notarizing(context) {
+    return;
+    
+  const { electronPlatformName, appOutDir } = context;  
+  if (electronPlatformName !== 'darwin') {//will be mas for mac app store
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  console.log("Notarizing...");
+
+  return await notarize({
+    appBundleId: 'org.simplewebserver.simplewebserver',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLEID,
+    appleIdPassword: "@keychain:ElectronSigningAppleID",
+    teamId: process.env.TEAMID,
+    ascProvider: process.env.TEAMID
+  });
+};
