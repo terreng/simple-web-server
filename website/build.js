3fc9b537823e2660dac9da35b48416f77b6660b5
Terren <11605395+terreng@users.noreply.github.com>
2022-06-20 11:07:10 -0700

    Fix version check url

--- website/build.js
+++ website/build.js
@@ -6,7 +6,7 @@ if (!fs.existsSync("src/.vuepress/dist/versions")){
 }
 var versions = JSON.parse(fs.readFileSync("versions.json", "utf8"));
 for (var i = 0; i < Object.keys(versions.versions).length; i++) {
-    fs.writeFileSync("src/.vuepress/dist/versions/"+Object.keys(versions.versions)[i]+".json", JSON.stringify((Object.keys(versions.versions)[i] !== versions.latest) ? {"update": true, "version": versions.latest, "name": versions.versions[versions.latest].name, "download": versions.versions[versions.latest].download} : {"update": false}), "utf8");
+    fs.writeFileSync("src/.vuepress/dist/versions/"+Object.keys(versions.versions)[i]+".json", JSON.stringify((Number(Object.keys(versions.versions)[i]) < Number(versions.latest)) ? {"update": true, "version": versions.latest, "name": versions.versions[versions.latest].name, "download": versions.versions[versions.latest].download} : {"update": false}), "utf8");
 }
 /*
 var static_files = [
