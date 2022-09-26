f63ca0a45bdfdead223d0e736ce99e84e3adf328
Ethan O'Brien <ethan.a.obrien@gmail.com>
2022-09-26 12:50:25 -0500

    Re-write translator.js

--- WSC.js
+++ WSC.js
@@ -5,24 +5,22 @@ WSC.createCrypto = require('./WSC/crypto.js');
 WSC.utils = require('./WSC/utils.js');
 WSC.httpRequest = require('./WSC/httpRequest.js');
 
-var a = require('./WSC/translator.js');
-WSC.onRequest = a.onRequest
-WSC.transformRequest = a.transformRequest
-WSC.HTTPRequest = a.HTTPRequest
+const a = require('./WSC/translator.js');
+WSC.onRequest = a.onRequest;
+WSC.transformRequest = a.transformRequest;
+WSC.HTTPRequest = a.HTTPRequest;
 
-var mime = require("./mime.js");
-WSC.MIMETYPES = mime.MIMETYPES
-WSC.MIMECATEGORIES = mime.MIMECATEGORIES
-WSC.HTTPRESPONSES = mime.HTTPRESPONSES
+const mime = require("./mime.js");
+WSC.MIMETYPES = mime.MIMETYPES;
+WSC.MIMECATEGORIES = mime.MIMECATEGORIES;
+WSC.HTTPRESPONSES = mime.HTTPRESPONSES;
 
-var handlers = require('./WSC/handlers.js');
-WSC.DirectoryEntryHandler = handlers.DirectoryEntryHandler
-WSC.BaseHandler = handlers.BaseHandler
+const handlers = require('./WSC/handlers.js');
+WSC.DirectoryEntryHandler = handlers.DirectoryEntryHandler;
+WSC.BaseHandler = handlers.BaseHandler;
 
-var main_fs = new WSC.FileSystem(__dirname)
+const main_fs = new WSC.FileSystem(__dirname)
 WSC.template_data = global.fs.readFileSync(global.path.resolve(__dirname, "directory-listing-template.html"), "utf8");
 WSC.static_template_data = global.fs.readFileSync(global.path.resolve(__dirname, "directory-listing-template-static.html"), "utf8");
 
 module.exports = WSC;
-
-
--- WSC/translator.js
+++ WSC/translator.js
@@ -1,104 +1,105 @@
-onRequest = function(serverconfig, req, res, FileSystem) {
-    WSC.transformRequest(req, res, serverconfig, function(requestApp) {
-        if (['GET','HEAD','PUT','POST','DELETE','OPTIONS'].includes(requestApp.request.method)) {
-            var handler = new WSC.DirectoryEntryHandler(FileSystem, requestApp.request, requestApp.app, req, res);
-            handler.tryHandle();
-        } else {
-            res.statusCode = 501;
-            res.statusMessage = 'Not Implemented';
-            res.end();
-        }
-    })
-}
-
-transformRequest = function(req, res, settings, callback) {
-    var curRequest = WSC.HTTPRequest({headers: req.headers,
-                                      method: req.method,
-                                      uri: req.url,
-                                      version: req.httpVersion,
-                                      ip: req.socket.remoteAddress})
-    var app = {
-        opts: settings
-    }
-    if (curRequest.method.toLowerCase() != 'put' && (curRequest.method.toLowerCase() != 'post' || (req.headers['content-type'] && req.headers['content-type'].startsWith('application/x-www-form-urlencoded')))) {
-        req.on('data', function(chunk) {
-            if (chunk && chunk != 'undefined') {
-                curRequest.body = Buffer.concat([curRequest.body, chunk])
-            }
-        })
-        req.on('end', function() {
-            curRequest.consumedRequest = true
-            if (curRequest.body.byteLength == 0) {
-                curRequest.body = null
+module.exports = {
+    onRequest: function(serverconfig, req, res, FileSystem) {
+        WSC.transformRequest(req, res, serverconfig, function(requestApp) {
+            if (['GET','HEAD','PUT','POST','DELETE','OPTIONS'].includes(requestApp.request.method)) {
+                const handler = new WSC.DirectoryEntryHandler(FileSystem, requestApp.request, requestApp.app, req, res);
+                handler.tryHandle();
             } else {
-                try {
-                    var ct = req.headers['content-type']
-                    if (ct) {
-                        var default_charset = 'utf-8'
-                        var ct = ct.toLowerCase()
-                        if (ct.startsWith('application/x-www-form-urlencoded')) {
-                            var charset_i = ct.indexOf('charset=')
-                            if (charset_i != -1) {
-                                var charset = ct.slice(charset_i + 'charset='.length,
-                                                       ct.length)
-                                console.log('using charset',charset)
-                            } else {
-                                var charset = default_charset
-                            }
-                            var bodydata = curRequest.body.toString(charset)
-                            var bodyparams = {}
-                            var items = bodydata.split('&')
-                            for (var i=0; i<items.length; i++) {
-                                var kv = items[i].replace(/\+/g, ' ').split('=')
-                                bodyparams[decodeURIComponent(kv[0])] = decodeURIComponent(kv[1])
-                            }
-                            curRequest.bodyparams = bodyparams
-                        }
-                    }
-                } catch(e) {
-                    curRequest.bodyparams = null
-                }
+                res.statusCode = 501;
+                res.statusMessage = 'Not Implemented';
+                res.end();
             }
-            callback({request: curRequest, app: app})
         })
-    } else {
-        curRequest.body = null
-        callback({request: curRequest, app: app})
-    }
-}
-
-HTTPRequest = function(opts) {
-    this.method = opts.method
-    this.uri = WSC.utils.relativePath(opts.uri, '')
-    this.ip = opts.ip
-    this.version = opts.version
-    this.headers = opts.headers
-    this.body = Buffer.from('')
-    this.bodyparams = null
-    this.consumedRequest = false
-
-    this.arguments = {}
-    var idx = this.uri.indexOf('?')
-    if (idx != -1) {
-        this.path = decodeURIComponent(this.uri.slice(0,idx))
-        var s = this.uri.slice(idx+1)
-        var parts = s.split('&')
-
-        for (var i=0; i<parts.length; i++) {
-            var p = parts[i]
-            var idx2 = p.indexOf('=')
-            this.arguments[decodeURIComponent(p.slice(0,idx2))] = decodeURIComponent(p.slice(idx2+1,s.length))
+    },
+    transformRequest: function(req, res, settings, callback) {
+        let curRequest = WSC.HTTPRequest({
+            headers: req.headers,
+            method: req.method,
+            uri: req.url,
+            version: req.httpVersion,
+            ip: req.socket.remoteAddress
+        })
+        let app = {
+            opts: settings
         }
-    } else {
-        this.path = decodeURIComponent(this.uri)
-    }
+        if (curRequest.method.toLowerCase() !== 'put' && req.headers['content-type'] && req.headers['content-type'].startsWith('application/x-www-form-urlencoded')) {
+            req.on('data', chunk => {
+                if (chunk) {
+                    curRequest.body = Buffer.concat([curRequest.body, chunk])
+                }
+            })
+            req.on('end', () => {
+                curRequest.consumedRequest = true;
+                if (curRequest.body.byteLength === 0) {
+                    curRequest.body = null;
+                } else {
+                    try {
+                        let ct = req.headers['content-type'];
+                        if (ct) {
+                            const default_charset = 'utf-8';
+                            ct = ct.toLowerCase();
+                            if (ct.startsWith('application/x-www-form-urlencoded')) {
+                                const charset_i = ct.indexOf('charset=');
+                                let charset;
+                                if (charset_i != -1) {
+                                    charset = ct.slice(charset_i + 'charset='.length, ct.length);
+                                    //console.log('using charset', charset);
+                                } else {
+                                    charset = default_charset;
+                                }
+                                const bodydata = curRequest.body.toString(charset);
+                                let bodyparams = {};
+                                const items = bodydata.split('&');
+                                for (var i=0; i<items.length; i++) {
+                                    const kv = items[i].replaceAll(/\+/g, ' ').split('=');
+                                    bodyparams[decodeURIComponent(kv[0])] = decodeURIComponent(kv[1]);
+                                }
+                                curRequest.bodyparams = bodyparams;
+                            }
+                        }
+                    } catch(e) {
+                        curRequest.bodyparams = null;
+                    }
+                }
+                callback({request: curRequest, app: app});
+            })
+        } else {
+            curRequest.body = null;
+            callback({request: curRequest, app: app});
+        }
+    },
+    HTTPRequest: function(opts) {
+        let rv = {
+            method: opts.method,
+            uri: WSC.utils.relativePath(opts.uri, ''),
+            ip: opts.ip,
+            version: opts.version,
+            headers: opts.headers,
+            body: Buffer.from(''),
+            bodyparams: null,
+            consumedRequest: false,
+            arguments: {}
+        }
+        var idx = rv.uri.indexOf('?');
+        if (idx !== -1) {
+            rv.path = decodeURIComponent(rv.uri.slice(0,idx));
+            const s = rv.uri.slice(idx+1);
+            const parts = s.split('&');
 
-    this.origpath = this.path
+            for (var i=0; i<parts.length; i++) {
+                const p = parts[i];
+                const idx2 = p.indexOf('=');
+                rv.arguments[decodeURIComponent(p.slice(0,idx2))] = decodeURIComponent(p.slice(idx2+1,s.length));
+            }
+        } else {
+            rv.path = decodeURIComponent(rv.uri);
+        }
 
-    if (this.path[this.path.length-1] == '/') {
-        this.path = this.path.slice(0,this.path.length-1)
+        rv.origpath = rv.path;
+
+        if (rv.path.endsWith('/')) {
+            rv.path = rv.path.slice(0, rv.path.length-1);
+        }
+        return rv;
     }
-    return this
 }
-
-module.exports = {onRequest: onRequest, transformRequest: transformRequest, HTTPRequest: HTTPRequest}
