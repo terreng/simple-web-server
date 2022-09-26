43a54c2414a30346765960a46437fa8a88470b40
Ethan O'Brien <ethan.a.obrien@gmail.com>
2022-09-26 11:52:52 -0500

    Re-write http request handler into a class

--- WSC/httpRequest.js
+++ WSC/httpRequest.js
@@ -1,160 +1,156 @@
-
-function httpRequest() {
-    this.onload = null
-    this.onerror = null
-    this.res = null
-    this.reDirected = false
-    this.reDirectCount = 0
-    this.reDirectLimit = 10
-    this.headers = {}
-    this.body = Buffer.from('')
-    this.streamToFile = false
-    this.savePath = null
-    this.handler = null
-    this.request = {}
-}
-
-httpRequest.prototype = {
-    setRequestHeader: function(k, v) {
-        this.headers[k] = v
-    },
-    setHeader: function(k, v) {
-        this.headers[k] = v
-    },
-    open: function(method, url, allowInsecure) {
-        if (! url.startsWith('http')) {
-            var error = 'url must start with http or https'
-            console.error(error)
-            if (this.onerror && typeof this.onerror == 'function') {
-                this.onerror(error)
-            } else if (this.onload && typeof this.onload == 'function') {
-                this.onload(error)
-            } else {
-                throw new Error(error)
-            }
-            return
+class httpRequest {
+    onload;
+    onerror;
+    res;
+    req;
+    reDirected;
+    reDirectCount;
+    reDirectLimit;
+    headers;
+    body;
+    streamToFile;
+    savePath;
+    handler;
+    request;
+    constructor() {
+        this.onload = null;
+        this.onerror = null;
+        this.res = null;
+        this.req = null;
+        this.reDirected = false;
+        this.reDirectCount = 0;
+        this.reDirectLimit = 10;
+        this.headers = {};
+        this.body = Buffer.from('');
+        this.streamToFile = false;
+        this.savePath = null;
+        this.handler = null;
+        this.request = {};
+    }
+    setRequestHeader(k, v) {
+        this.headers[k] = v;
+    }
+    setHeader(k, v) {
+        this.headers[k] = v;
+    }
+    open(method, inUrl, allowInsecure) {
+        const url = new URL(inUrl);
+        if (!['https:', 'http:'].includes(url.protocol)) {
+            const error = new Error('Url must start with http or https');
+            this.error(error);
+            return;
         }
-        this.method = method
-        const { port, pathname, search, protocol, host } = new URL(url)
-        var path = pathname + search
-        if (protocol =='https:') {
-            if (allowInsecure !== true) {
-                this.req = https.request({method: method, protocol: protocol, host: host, path: path, port: port || 443})
-            } else {
-                this.req = https.request({method: method, protocol: protocol, host: host, path: path, port: port || 443, rejectUnauthorized: false, requestCert: true })
+        this.method = method;
+        const { port, pathname, search, protocol, host } = url;
+        const path = pathname + search;
+        let opts = {method: method, protocol: protocol, host: host, path: path};
+        if (protocol === 'https:') {
+            opts.port = port || 443;
+            if (allowInsecure === true) {
+                opts.rejectUnauthorized = false;
+                opts.requestCert = true;
             }
+            this.req = https.request(opts);
         } else {
-            this.req = http.request({method: method, protocol: protocol, host: host, path: path, port: port || 80})
+            opts.port = port || 80;
+            this.req = http.request(opts);
         }
-        this.req.on('error', function(error) {
-            if (this.onerror && typeof this.onerror == 'function') {
-                this.onerror(error)
-            } else if (this.onload && typeof this.onload == 'function') {
-                this.onload(error)
-            }
-        }.bind(this))
-    },
-    send: function(data) {
+        this.req.on('error', this.error.bind(this))
+    }
+    send(data) {
         try {
-            for (var k in this.headers) {
-                this.req.setHeader(k, this.headers[k])
+            for (const k in this.headers) {
+                this.req.setHeader(k, this.headers[k]);
             }
             if (data) {
-                if (typeof data == 'string') {
-                    var data = Buffer.from(data)
-                } else if (data instanceof ArrayBuffer) {
-                    var data = Buffer.from(data)
+                if (!Buffer.isBuffer(data)) {
+                    data = Buffer.from(data);
                 }
-                this.responseData = data
-                this.req.setHeader('content-length', data.byteLength)
+                this.responseData = data;
+                this.req.setHeader('content-length', data.byteLength);
             }
-            this.req.on('response', this.onResponse.bind(this))
-            if (this.responseData) {
-                this.req.write(this.responseData)
-            }
-            this.req.end()
-        } catch(error) {
-            if (this.onerror && typeof this.onerror == 'function') {
-                this.onerror(error)
-            } else if (this.onload && typeof this.onload == 'function') {
-                this.onload(error)
-            }
+            this.req.on('response', this.onResponse.bind(this));
+            if (this.responseData) this.req.write(this.responseData);
+            this.req.end();
+        } catch(err) {
+            this.error(err);
         }
-    },
-    onResponse: function(res) {
-        res.on('error', function(error) {
-            if (this.onerror && typeof this.onerror == 'function') {
-                this.onerror(error)
-            } else if (this.onload && typeof this.onload == 'function') {
-                this.onload(error)
-            }
-        }.bind(this))
-        this.res = res
-        if ((res.statusCode == 301 || res.statusCode == 302 || res.statusCode == 307) && this.reDirectLimit > this.reDirectCount && res.headers.location) {
-            var request = new WSC.httpRequest()
-            request.streamToFile = this.streamToFile
-            request.savePath = this.savePath
-            request.handler = this.handler
-            request.reDirectCount = this.reDirectCount + 1
-            request.reDirected = true
-            request.headers = this.headers
-            request.onload = this.onload
-            request.open(this.method, res.headers.location)
-            request.send(this.responseData || undefined)
-            return
+    }
+    onResponse(res) {
+        res.on('error', this.error.bind(this));
+        this.res = res;
+        if ([301, 302, 303, 307, 308].includes(res.statusCode) && this.reDirectLimit > this.reDirectCount && res.headers.location) {
+            const request = new WSC.httpRequest();
+            request.streamToFile = this.streamToFile;
+            request.savePath = this.savePath;
+            request.handler = this.handler;
+            request.reDirectCount = this.reDirectCount + 1;
+            request.reDirected = true;
+            request.headers = this.headers;
+            request.onload = this.onload;
+            request.open(this.method, res.headers.location);
+            request.send(this.responseData || undefined);
+            return;
         }
-        if (! this.streamToFile) {
-            res.on('data', (chunk) => {
-                this.body = Buffer.concat([this.body, chunk])
+        if (!this.streamToFile) {
+            res.on('data', chunk => {
+                this.body = Buffer.concat([this.body, chunk]);
             })
             res.on('end', () => {
-                var evt = {target: {headers:this.res.headers,
-                                    code:this.res.statusCode,
-                                    status:this.res.statusCode,
-                                    responseHeaders:this.res.rawHeaders,
-                                    responseHeadersParsed:this.res.headers,
-                                    response:this.body,
-                                    redirected:this.reDirected}
-                          }
-                if (this.onload && typeof this.onload == 'function') {
-                    this.onload(evt)
+                const evt = {
+                    target: {
+                        headers: this.res.headers,
+                        code: this.res.statusCode,
+                        status: this.res.statusCode,
+                        responseHeaders: this.res.rawHeaders,
+                        responseHeadersParsed: this.res.headers,
+                        response: this.body,
+                        redirected: this.reDirected
+                    }
                 }
+                if (typeof this.onload === 'function') this.onload(evt);
             })
         } else {
-            if (! this.savePath.startsWith('/')) {
-                this.savePath = WSC.utils.relativePath(this.savePath, WSC.utils.stripOffFile(this.handler.request.origpath))
+            if (!this.savePath.startsWith('/')) {
+                this.savePath = WSC.utils.relativePath(this.savePath, WSC.utils.stripOffFile(this.handler.request.origpath));
             }
-            var writeStream = this.handler.fs.createWriteStream(this.savePath)
-            writeStream.on('error', function (err) {
-                var evt = {error: err}
-                if (this.onerror && typeof this.onerror == 'function') {
-                    this.onerror(evt)
-                } else if (this.onload && typeof this.onload == 'function') {
-                    this.onload(evt)
+            const writeStream = this.handler.fs.createWriteStream(this.savePath);
+            writeStream.on('error', this.error.bind(this));
+            this.res.pipe(writeStream);
+            this.res.on('end', () => {
+                const evt = {
+                    target: {
+                        headers: this.res.headers,
+                        code: this.res.statusCode,
+                        status: this.res.statusCode,
+                        responseHeaders: this.res.rawHeaders,
+                        responseHeadersParsed: this.res.headers,
+                        response: 'The response was written to a file.',
+                        redirected: this.reDirected
+                    }
                 }
-            }.bind(this))
-            this.res.pipe(writeStream)
-            this.res.on('end', function () {
-                var evt = {target: {headers:this.res.headers,
-                                    code:this.res.statusCode,
-                                    status:this.res.statusCode,
-                                    responseHeaders:this.res.rawHeaders,
-                                    responseHeadersParsed:this.res.headers,
-                                    response:'The response was written to a file.',
-                                    redirected:this.reDirected}
-                          }
-                if (this.onload && typeof this.onload == 'function') {
-                    this.onload(evt)
-                }
-            }.bind(this))
+                if (typeof this.onload == 'function') this.onload(evt);
+            })
             
         }
-    },
-    setupStreamToFile: function(handler, savePath) {
-        this.streamToFile = true
-        this.handler = handler
-        this.savePath = savePath
     }
+    error(error) {
+        if (typeof this.onerror === 'function') {
+            this.onerror(error);
+        } else if (typeof this.onload === 'function') {
+            this.onload(error);
+        } else {
+            throw error;
+        }
+    }
+    setupStreamToFile(handler, savePath) {
+        if (!handler) throw new Error('Missing handler!');
+        if (!savePath) throw new Error('Missing savePath!');
+        this.streamToFile = true;
+        this.handler = handler;
+        this.savePath = savePath;
+    }
+    
 }
 
 function testHttpRequest() {
