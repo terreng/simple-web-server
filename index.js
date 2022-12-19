e44ec57f674afdcb1b6624c9b1de0e227ffff416
Terren <11605395+terreng@users.noreply.github.com>
2022-12-18 19:46:08 -0500

    Add restartServersWithPlugins

--- index.js
+++ index.js
@@ -484,8 +484,8 @@ function createServers() {
     updateServerStates();
 }
 
-function startServers() {
-    if (running_servers.length <= 0) {
+function startServers(force_restart_indexes) {
+    if (running_servers.length == 0) {
         createServers();
         return;
     }
@@ -500,7 +500,7 @@ function startServers() {
             }
         }
 
-        if (found_matching_config) {
+        if (found_matching_config && (force_restart_indexes || []).indexOf(i) == -1) {
             need_close_servers--;
         } else {
             running_servers[i].deleted = true;
@@ -538,6 +538,21 @@ function configsEqual(config1, config2) {
         return true;
     }
     return false;
+}
+
+function restartServersWithPlugins(pluginids) {
+    var server_indexes = [];
+    for (let i=0; i<running_servers.length; i++) {
+        if (running_servers[i].plugins) {
+            for (let e=0; e<pluginids.length; e++) {
+                if (running_servers[i].plugins[pluginids[e]] && running_servers[i].plugins[pluginids[e]].enabled) {
+                    server_indexes.push(i);
+                    break;
+                }
+            }
+        }
+    }
+    startServers(server_indexes);
 }
 
 let update_info;
