c6b4adf2a22922f01be922f803d1f9a08e05cc6d
Terren <11605395+terreng@users.noreply.github.com>
2023-03-23 19:49:10 -0400

    Version 1.2.7

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const version = 1002006;
+const version = 1002007;
 const install_source = "website"; //"website" | "microsoftstore" | "macappstore"
 const {app, BrowserWindow, ipcMain, Menu, Tray, dialog, shell, nativeTheme} = require('electron');
 const {networkInterfaces} = require('os');
