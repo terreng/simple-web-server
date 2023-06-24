cbfe347d2df5f887a97448692f5445daa7370aef
Terren <11605395+terreng@users.noreply.github.com>
2023-06-24 12:37:42 -0700

    Add meta keywords

--- website/src/.vuepress/config.js
+++ website/src/.vuepress/config.js
@@ -11,7 +11,7 @@ module.exports = {
     ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
     ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
     ['link', { rel: 'shortcut icon', href: '/favicon.ico' }],
-    ['meta', { name: 'keywords', content: 'Simple Web Server,web server,Web Server for Chrome,local web server' }],
+    ['meta', { name: 'keywords', content: 'Simple Web Server,web server,Web Server for Chrome,local web server,200 ok,server' }],
     ['script', { type: 'module', src: '/ms-store-badge.bundled.js' }]
   ],
   locales: {
